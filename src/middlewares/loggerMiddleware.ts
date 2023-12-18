eeea5fcbb331e47452b618dcf8a8ab8f0cd629ed
Ivan Tyulkin <phantomlancerinyourmind@gmail.com>
2023-12-18 19:25:59 +0300

    [fix] logger username

--- src/middlewares/loggerMiddleware.ts
+++ src/middlewares/loggerMiddleware.ts
@@ -5,10 +5,13 @@ import { CallbackFunction } from "../types/functions";
 export const loggerMiddleware = async (ctx: Context, next: CallbackFunction) => {
   if (ctx.message) {
     const message = ctx.message;
+    const from = message.from.username
+      ? "from: " + message.from.username
+      : "from id: " + message.from.id;
     console.log(
       `new message
        id: ${message.message_id}
-       from: ${message.from.username}\n`,
+       ${from}\n`,
     );
   }
   next();
